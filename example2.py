da2d75a0ea8d584f505b9fafaa12c504cbbeda32
Samuel Annan <samuelannan191@gmail.com>
2024-03-25 22:12:55 -0700

    area of a circle and square

---
+++ example2.py
@@ -0,0 +1,23 @@
+# write a python program to calculate the area of a circle
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius * radius
+print("Area of the circle is: %.2f" %area)
+# Path: example3.py
+# //write a python program to calculate the area of a rectangle
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+area = length * breadth
+print("Area of the rectangle is: %.2f" %area)
+# Path: example4.py
+# //write a python program to calculate the area of a triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+
+print("Area of the triangle is: %.2f" %area)
+# Path: example5.py
+# //write a python program to calculate the area of a square
+side = float(input("Enter the side of the square: "))
+area = side * side
+print("Area of the square is: %.2f" %area)
