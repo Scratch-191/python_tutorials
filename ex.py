b5d22a921751d9c0bdbe1f61de14f3f5b522a964
Samuel Annan <samuelannan191@gmail.com>
2024-03-27 22:44:54 -0700

    reading the number of lines in a file

--- ex.py
+++ ex.py
@@ -1,14 +1,24 @@
 
-# Path: code4.py
-# Write your code here
-#
-# Path: ex.py
-# Compare this snippet from code4.py:   
 
-# Path: code4.py
+# Path: code3.py
+import sys
+try:
+    file_name = sys.argv[1]
+except IndexError:
+    print("Please provide a file name as a command-line argument.")
+    sys.exit()
+try:
+    with open(file_name) as file:
+        lines = file.readlines()
+        print("Number of lines in the file:", len(lines))
+except FileNotFoundError:
+    print("File not found.")
+    sys.exit()
+except UnicodeDecodeError:
+    print("Cannot read the file as it is not a text file.")
+    sys.exit()
+except Exception as e:
+    print(e)
+    sys.exit()
+    
 
-file = open("file.txt", "r")
-lines = file.readlines()
-print(len(lines))
-file.close()
-#
