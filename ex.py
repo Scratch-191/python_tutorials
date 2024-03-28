f0278fbd51dd98d198f2d2274b7594c1e01adbfd
Samuel Annan <samuelannan191@gmail.com>
2024-03-27 22:41:41 -0700

    reading lines in a file

---
+++ ex.py
@@ -0,0 +1,14 @@
+
+# Path: code4.py
+# Write your code here
+#
+# Path: ex.py
+# Compare this snippet from code4.py:   
+
+# Path: code4.py
+
+file = open("file.txt", "r")
+lines = file.readlines()
+print(len(lines))
+file.close()
+#
